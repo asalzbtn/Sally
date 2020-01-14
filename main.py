1dc0aeb040d50c5b93553782bc4b84a4a0da4fcd
asalzbtn <asal.zabetian-hosseini@mail.mcgill.ca>
2020-01-14 18:32:43 -0500

    Signed-off-by: asalzbtn <asal.zabetian-hosseini@mail.mcgill.ca>

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 ##Initiate Communications with the simulator
 ## CHANGE THE IP ADDRESS ACCORDINGLY
 ## (The IP address is the one from the raspberry PI)
-comm = Ethernet(50000,'192.168.22.143')
+comm = Ethernet(45000,'132.206.62.246')
 
 time.sleep(1)
 counter = 0
@@ -26,7 +26,7 @@ while True:
     
     ##Send commands to the simulator
     comm.send([12, 24, 45, counter])
-    print('commands:', end="")
+    print('commands:')
     print(str([12, 24, 45, counter]))
 		
     time.sleep(1)
