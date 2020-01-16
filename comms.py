0fd0e01278cf660c4a84503f75be919a4b64918a
asalzbtn <asal.zabetian-hosseini@mail.mcgill.ca>
2020-01-15 20:28:04 -0500

    Signed-off-by: asalzbtn <asal.zabetian-hosseini@mail.mcgill.ca>

--- comms.py
+++ comms.py
@@ -1,69 +1,62 @@
+#OPAL-RT Technologies Inc.
+#This script is provided 'as is', without warranty of any kind.
+
+#Purpose: Communication file responsible for UDP communications with the simulator
+#The raspberry pi acts as a server, and the simulator is a client.
+
 import socket
 import array
+import pickle
 
 class Ethernet:
-    
-    BUF_SIZE = 4096  ## 4096
-    HOST_IP = '132.206.62.246'
-    PORT = 45000
-    
-    
-    def __init__(self):
+
+	BUF_SIZE = 1024
+	HOST_IP = '132.206.62.246'
+	PORT = 45000
+	def __init__(self,port,HOST_IP):
+        
+		# Set up socket and bind socket to port and local host IP
+		self.PORT = port
+		self.HOST_IP = HOST_IP
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.s.bind((self.HOST_IP, self.PORT))
+		print('Waiting for connection from simulator...')
+		data, self.address = self.s.recvfrom(self.BUF_SIZE)
+		print ('Connected')
+		self.message_header = data[0:6]
+		
+		self.s.close()
+		
+	def status(self):
+	# Set up socket and bind socket to port and local host IP
+
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		self.s.bind((self.HOST_IP, self.PORT))
+		
+		data, address = self.s.recvfrom(self.BUF_SIZE)
+		# Rearrange data from bytes into an array, d for double
+		data_doubles = array.array('d', data)
+
+		self.s.close()
+		return data_doubles[1:]
+	
+
+	def send(self,commands):
         # Set up socket and bind socket to port and local host IP
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind((self.HOST_IP, self.PORT))
-        
-        # Receive first message from microgrid to acquire message identification
-        print('Waiting for connection from microgrid...')
-        data, self.address = self.s.recvfrom(self.BUF_SIZE)
-        data_doubles = array.array('d', data)
-        self.message_header = data_doubles[0]
-        print('Connected!')
-        
-        
-        # Close socket to prevent accumulation of data
-        self.s.close()
-        
-    def status(self):
-        # Set up socket and bind socket to port and local host IP
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind((self.HOST_IP, self.PORT))
-        
-        # Receive latest microgrid data
-        data, self.address = self.s.recvfrom(self.BUF_SIZE)
-        # Rearrange data from bytes into an array
-        data_doubles = array.array('d', data)
-        
-        # Close socket to prevent accumulation of data
-        self.s.close()
-#        return (data_doubles[0],data_doubles[1])
-        return data_doubles[1:]
-    
-    
-    def send(self, commands):  # Why we need commands here? Command is the data from status.
-        # Set up socket and bind socket to port and local host IP
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind((self.HOST_IP, self.PORT))
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		self.s.bind((self.HOST_IP, self.PORT))
         
         # Rearrange data from array and include message identification
-        #n = len(commands)
-        
-#        message=array.array('d', [self.message_header])
-        message=array.array('d',[self.message_header])
-#        message_length = array.array('d', [commands]) # h represent unsinged short
-#        message=array.array('d',[])
-#        for i in range(1):
-        message.append(commands[0])
-        message.append(commands[1])
-        message.append(commands[2])
-        message.append(commands[3])
-        message.append(commands[4])
-        message.append(commands[5])
-        message.append(commands[6])
-        # message.append( message_length)
-        
-        # Send data
-        self.s.sendto(bytes(message), self.address)
-        
+		n = len(commands)
+		message_length = bytes(array.array('h', [n*8])) 
+
+		message = self.message_header + message_length+ bytes(array.array('d',commands))
+		
+		
+		self.s.sendto(message, self.address)
+		
         # Close socket to prevent accumulation of data
-        self.s.close()
+		self.s.close()
+
---
+++ comms_1.py
@@ -0,0 +1,74 @@
+import socket
+import array
+
+class Ethernet:
+    
+    BUF_SIZE = 4096  ## 4096
+    HOST_IP = '132.206.62.246'
+    PORT = 45000
+    
+    
+    def __init__(self):
+        # Set up socket and bind socket to port and local host IP
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind((self.HOST_IP, self.PORT))
+        
+        # Receive first message from microgrid to acquire message identification
+        print('Waiting for connection from microgrid...')
+        data, self.address = self.s.recvfrom(self.BUF_SIZE)
+        data_doubles = array.array('d', data)
+        self.message_header = data_doubles[0]
+        print('Connected!')
+        
+        
+        # Close socket to prevent accumulation of data
+        self.s.close()
+        
+    def status(self):
+        # Set up socket and bind socket to port and local host IP
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind((self.HOST_IP, self.PORT))
+        
+        # Receive latest microgrid data
+        data, self.address = self.s.recvfrom(self.BUF_SIZE)
+        # Rearrange data from bytes into an array
+        data_doubles = array.array('d', data)
+        
+        # Close socket to prevent accumulation of data
+        self.s.close()
+#        return (data_doubles[0],data_doubles[1])
+        return data_doubles[1:]
+    
+    
+    def send(self, commands):  # Why we need commands here? Command is the data from status.
+        # Set up socket and bind socket to port and local host IP
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind((self.HOST_IP, self.PORT))
+        
+        # Rearrange data from array and include message identification
+        #n = len(commands)
+        
+#        message=array.array('d', [self.message_header])
+        message=array.array('d',[self.message_header])
+#        message_length = array.array('d', [commands]) # h represent unsinged short
+#        message=array.array('d',[])
+#        for i in range(1):
+#        message.append(commands[0])
+#        message.append(commands[1])
+#        message.append(commands[2])
+#        message.append(commands[3])
+#        message.append(commands[4])
+#        message.append(commands[5])
+#        message.append(commands[6])
+        # message.append( message_length)
+        
+        n = len(commands)
+		message_length = bytes(array.array('h', [n*8])) 
+
+		message = self.message_header + message_length+ bytes(array.array('d',commands))
+        
+        # Send data
+        self.s.sendto(bytes(message), self.address)
+        
+        # Close socket to prevent accumulation of data
+        self.s.close()
